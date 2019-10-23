6d63fa08f0abb1108da92c9f0fd8f92bd8c830ca
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-10-23 13:01:35 -0600

    modified enrollInSummerSchool.js

--- enrollInSummerSchool.js
+++ enrollInSummerSchool.js
@@ -1,21 +1,29 @@
+'use strict';
 const studentData = [
-    {
-      name: 'Tim',
-      status: 'Current student',
-      course: 'Biology',
-    },
-    {
-      name: 'Sue',
-      status: 'Withdrawn',
-      course: 'Mathematics',
-    },
-    {
-      name: 'Liz',
-      status: 'On leave',
-      course: 'Computer science',
-    },
-  ];
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
   
-  function enrollInSummerSchool(students) {
-    // your code here
-  }
+function enrollInSummerSchool(students) {
+  return students.map(student => {
+    return {
+      name: student.name,
+      status: 'In Summer school',
+      course: student.course,
+    };
+  });
+}
+console.log(enrollInSummerSchool(studentData));
