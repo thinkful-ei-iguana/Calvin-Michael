abc76314d6cee4692a75f6a6f9c1541e4ed24944
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-10-23 12:49:23 -0600

    added enrollInSummerSchool.js

---
+++ enrollInSummerSchool.js
@@ -0,0 +1,21 @@
+const studentData = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology',
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Liz',
+      status: 'On leave',
+      course: 'Computer science',
+    },
+  ];
+  
+  function enrollInSummerSchool(students) {
+    // your code here
+  }
